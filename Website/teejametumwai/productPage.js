4f3f761674468d71da8da1a96233905b1384b5fc
Chirakan <64070130@it.kmitl.ac.th>
2022-11-13 18:47:32 +0700

    Update productPage.js

--- Website/teejametumwai/productPage.js
+++ Website/teejametumwai/productPage.js
@@ -1,35 +1,35 @@
 function loadJSON() {
-    let page = localStorage.productPage
     fetch('menu.json')
         .then(response => response.json())
         .then(data => {
             let productPage = '';
-            productPage += `
-            <div class="main_left">
-                <img class="main_left_img" src="img/${data[page].img1}" alt="cat-1" />
-                <div class=" main_left_secimg">
-                    <img class="img" src="img/${data[page].img1}" alt="cat-1" />
-                    <img class="img" src="img/${data[page].img2}" alt="cat-1" />
-                    <img class="img" src="img/${data[page].img3}" alt="cat-1" />
-                    <img class="img" src="img/${data[page].img4}" alt="cat-1" />
+            let img = '';
+            for (i = 0; i < data[localStorage.productPage].img.length; i++) {
+                img += `<img class="img" src="img/${data[localStorage.productPage].img[i]}" alt="cat-1" />`;
+            }
+
+                productPage += `
+                    <div class="main_left">
+                    <img class="main_left_img" src="img/${data[localStorage.productPage].img[0]}" alt="cat-1" />
+                    <div class=" main_left_secimg">
+                        ${img}
                 </div>
-            </div>
-            <div class="main_right">
-                <h4 class="category">${data[page].category}</h4>
-                <h2 class="main_right_name">${data[page].name}</h2>
-                <h3>${data[page].price} THB</h3>
-                <div class="main_right_detail ">${data[page].detail}
-                <ul>
-                    <li>องุ่นกลมเหมือนคุกกี้</li>
-                    <li>องุ่นกลมเหมือนคุกกี้</li>
-                    <li>องุ่นกลมเหมือนคุกกี้</li>
-                    <li>องุ่นกลมเหมือนคุกกี้</li>
-                </ul>
+                <div class="main_right">
+                    <h4 class="category">${data[localStorage.productPage].category}</h4>
+                    <h2 class="main_right_name">${data[localStorage.productPage].name}</h2>
+                    <h3>${data[localStorage.productPage].price} THB</h3>
+                    <div class="main_right_detail ">${data[localStorage.productPage].detail}
+                    <ul>
+                        <li>องุ่นกลมเหมือนคุกกี้</li>
+                        <li>องุ่นกลมเหมือนคุกกี้</li>
+                        <li>องุ่นกลมเหมือนคุกกี้</li>
+                        <li>องุ่นกลมเหมือนคุกกี้</li>
+                    </ul>
+                    </div>
+                    <button data-id="${data[localStorage.productPage].id}" type="button" onClick="addToCart(this)" class="addtocart btn">Add To Cart</button>
+                    <button type="button" onClick="calculateCart()" class="addtocart btn">Check</button>
                 </div>
-                <button data-id="${data[page].id}" type="button" onClick="addToCart(this)" class="addtocart btn">Add To Cart</button>
-                <button type="button" onClick="calculateCart()" class="addtocart btn">Check</button>
-            </div>
-        `;
+            `;
 
             document.getElementsByClassName("main_detail_1")[0].innerHTML = productPage;
 
