90cf8a5755abdb13921a93666ee6fec72cbffb45
NutThai <88415649+NutThai@users.noreply.github.com>
2022-11-14 20:43:39 +0700

    category

--- Website/teejametumwai/categoryPage.js
+++ Website/teejametumwai/categoryPage.js
@@ -153,7 +153,40 @@ function loadJSON() {
 
             //     });
             // }
-            
+            else{
+                if(localStorage.categoryPage == "cake"){
+                    document.getElementsByClassName("nameofcate")[0].innerHTML = "<h1>Cake</h1>"
+                    document.getElementsByClassName("main_content_2")[0].innerHTML = cake
+                }
+                if(localStorage.categoryPage == "cookie"){
+                    document.getElementsByClassName("nameofcate")[0].innerHTML = "<h1>Cookie</h1>"
+                    document.getElementsByClassName("main_content_2")[0].innerHTML = cookie
+                }
+                if(localStorage.categoryPage == "bread"){
+                    document.getElementsByClassName("nameofcate")[0].innerHTML = "<h1>Bread</h1>"
+                    document.getElementsByClassName("main_content_2")[0].innerHTML = bread
+                }
+                if(localStorage.categoryPage == "donut"){
+                    document.getElementsByClassName("nameofcate")[0].innerHTML = "<h1>Donut</h1>"
+                    document.getElementsByClassName("main_content_2")[0].innerHTML = donut
+                }
+                if(localStorage.categoryPage == "coffee"){
+                    document.getElementsByClassName("nameofcate")[0].innerHTML = "<h1>Coffee</h1>"
+                    document.getElementsByClassName("main_content_2")[0].innerHTML = coffee
+                }
+                if(localStorage.categoryPage == "milk"){
+                    document.getElementsByClassName("nameofcate")[0].innerHTML = "<h1>Milk</h1>"
+                    document.getElementsByClassName("main_content_2")[0].innerHTML = milk
+                }
+                if(localStorage.categoryPage == "tea"){
+                    document.getElementsByClassName("nameofcate")[0].innerHTML = "<h1>Tea</h1>"
+                    document.getElementsByClassName("main_content_2")[0].innerHTML = tea
+                }
+                if(localStorage.categoryPage == "soda"){
+                    document.getElementsByClassName("nameofcate")[0].innerHTML = "<h1>Soda</h1>"
+                    document.getElementsByClassName("main_content_2")[0].innerHTML = soda
+                }
+            }
         })
         .catch(error => {
             alert(`User live server or local server`);
