4ff3c2697c5c1c76286f81f471129e68dbd1c8bd
Chirakan <64070130@it.kmitl.ac.th>
2022-11-15 23:30:12 +0700

    add func

--- Website/teejametumwai/scripts.js
+++ Website/teejametumwai/scripts.js
@@ -14,6 +14,8 @@ const whenuser = document.querySelector(".whenuser");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 
+const dot = document.querySelector(".dot");
+
 const content2 = document.querySelector(".main_content_2_flex");
 let count = 0;
 
@@ -86,14 +88,19 @@ function showSlides() {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = " 0";
+    slides[i].style.opacity = "0";
   }
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.opacity = " 1";
+  slides[slideIndex - 1].style.opacity = "1";
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+function currentSlide(num){
+  slideIndex = num;
+  console.log(slideIndex);
 }
